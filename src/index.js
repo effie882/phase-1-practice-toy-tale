376799776755deae571f204cd643a23fde53e3e2
effie882 <effiemorara@gmail.com>
2025-06-29 14:21:25 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -12,4 +12,78 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  fetch("http://localhost:3000/toys")
+    .then((res) => res.json())
+    .then((toys) => {
+      toys.forEach(renderToy);
+    });
+
+
+  function renderToy(toy) {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h2>${toy.name}</h2>
+      <img src="${toy.image}" class="toy-avatar" />
+      <p>${toy.likes} Likes</p>
+      <button class="like-btn" id="${toy.id}">Like ❤️</button>
+    `;
+
+    const likeBtn = card.querySelector(".like-btn");
+    likeBtn.addEventListener("click", () => handleLike(toy, card));
+
+    toyCollection.appendChild(card);
+  }
+
+ 
+  toyForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+
+    const toyData = {
+      name,
+      image,
+      likes: 0
+    };
+
+    fetch("http://localhost:3000/toys", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify(toyData)
+    })
+      .then((res) => res.json())
+      .then((newToy) => {
+        renderToy(newToy);
+        toyForm.reset();
+      });
+  });
+
+
+  function handleLike(toy, card) {
+    const newLikes = toy.likes + 1;
+
+    fetch(`http://localhost:3000/toys/${toy.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: JSON.stringify({
+        likes: newLikes
+      })
+    })
+      .then((res) => res.json())
+      .then((updatedToy) => {
+        toy.likes = updatedToy.likes;
+        card.querySelector("p").textContent = `${updatedToy.likes} Likes`;
+      });
+  }
 });
+
